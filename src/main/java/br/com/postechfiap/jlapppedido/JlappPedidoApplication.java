bb50c9b8702a0a6bf347c4774e19d7e0e8d0aecf
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-05-05 10:37:49 -0300

    first commit

---
+++ src/main/java/br/com/postechfiap/jlapppedido/JlappPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.jlapppedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlappPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlappPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postechfiap/jlapppedido/JlappPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.jlapppedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlappPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
